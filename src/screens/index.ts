0109eb8c0a2a8ffb399a74dee98dab7c7284d8a5
Jose Reyes <jherrera11@ucol.mx>
2024-02-24 09:36:06 -0600

    :sparkles: Import new screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,7 +1,10 @@
 import SignInScreen from './auth/SignInScreen';
 import LoginScreen from './auth/LoginScreen';
 import SplashScreen from './SplashScreen';
+import HomeScreen from './HomeScreen';
+
 export {
+  HomeScreen,
   SplashScreen,
   SignInScreen,
   LoginScreen,
