0df5db34872a9668b54ec2f4c7542570e2261aa0
Oliver Visoso <ovisosodev@gmail.com>
2024-02-23 19:16:15 -0600

    add navigation on screens

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import 'react-native-gesture-handler';
+import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import {SplashScreen} from './src/screens';
-import Onboarding from './src/screens/OnboardingScreen';
+import StackNavigation from './src/routes/StackNavigation';
 
 function App(): React.JSX.Element {
-  const [visible, setVisible] = useState<boolean>(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setVisible(false);
-    }, 2000);
-  }, []);
-
   return (
     <SafeAreaView style={styles.app}>
-      {visible && <SplashScreen />}
-      <Onboarding />
+      <StackNavigation />
     </SafeAreaView>
   );
 }
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,8 +1,10 @@
 import SignInScreen from './auth/SignInScreen';
 import LoginScreen from './auth/LoginScreen';
 import SplashScreen from './SplashScreen';
+import Onboarding from './OnboardingScreen';
 export {
   SplashScreen,
   SignInScreen,
   LoginScreen,
+  Onboarding,
 }
