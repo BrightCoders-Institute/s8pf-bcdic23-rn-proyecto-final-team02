20c68bdddddb6d18e63fd563b337182c3b436d1a
Jose Reyes <jherrera11@ucol.mx>
2024-02-24 09:31:31 -0600

    :tada: add new folder data

---
+++ src/data/CompanyData.ts
@@ -0,0 +1,14 @@
+export const CompanyData =[
+    {
+        id:'1',
+        name:'PepsiCo',
+        picture: require('../assets/pepsi-logo.jpg'),
+        distancing:'2.3 km from you ',
+    },
+    {
+        id:'2',
+        name:'The Coca-Cola Company',
+        picture: require('../assets/cocacola-logo.webp'),
+        distancing:'2.3 km from you',
+    }
+]
