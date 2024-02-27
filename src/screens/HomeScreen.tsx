f31dc9a9d68cd644908ebe1ffb00f96629ca91c9
Jose Reyes <jherrera11@ucol.mx>
2024-02-26 18:23:16 -0600

    :art: Upgrade indentation , black spaces and change const

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,18 +5,19 @@ import {CardWorkCompoment,CardCompanyCompoment} from '../components';
 import { WorkData } from '../data/WorkData';
 import { CompanyData } from '../data/CompanyData';
 
-
 const HomeScreen = () => {
-
+  const user_male=require('../assets/user-male-avatar.png')
   return (
     <View style={styles.container}>
       <View style={{ marginLeft: 14, marginTop: 77 }}>
+        
         <TextComponent
           text="Hello Jonathan,"
           font="100"
           color="black"
           size={22}
         />
+
         <TextComponent
           styles={{ marginTop: 9 }}
           text="Jobs wait for you"
@@ -24,10 +25,12 @@ const HomeScreen = () => {
           color="black"
           size={22}
         />
+
         <Image
-          source={require('../assets/user-male-avatar.png')}
+          source={user_male}
           style={{ width: 73, height: 75, marginLeft: 276, position: 'absolute' }}
         />
+
         <View style={{ height: 10, width: 196, backgroundColor: '#3825AE', marginBottom: 14, marginTop: 3 }} />
         <View style={{ height: 10, width: 103, backgroundColor: '#3825AE', marginBottom: 23 }} />
 
@@ -48,8 +51,6 @@ const HomeScreen = () => {
              renderItem={({ item }) => <CardWorkCompoment work = {item} />}
             />
 
-
-
         <TextComponent
         styles={{marginBottom:16 ,marginTop:25}}
           text="Nearby Businesses"
@@ -64,28 +65,20 @@ const HomeScreen = () => {
             renderItem={({ item }) => <CardCompanyCompoment company = {item} />}
           />
 
-
          <View style={{marginTop:11}}>
 
           <NavegatorComponent  />
 
          </View>
-
-
-
       </View>
-
-
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
     margin: 16
   },
 });
-
 
 export default HomeScreen;
