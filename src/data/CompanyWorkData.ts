9b5cb53a4df290f1854fbc2d155bb42aed5c219a
Jose Reyes <jherrera11@ucol.mx>
2024-03-05 16:50:16 -0600

    :tada: Add new data

---
+++ src/data/CompanyWorkData.ts
@@ -0,0 +1,36 @@
+export const CompanyWorkData = [
+    {
+        id:'1',
+        logo: require('../assets/pepsi-logo.jpg'),
+        branch:'Colimas Branch',
+        work: require('../assets/img/PepsiCo-work-1.jpg'),
+        job:'Truck driver',
+        salary:'MX 2000',
+        date:'Monday to Friday',
+        time:'7:00 am to 3:00 pm',
+    },
+    {
+        id:'2',
+        logo:require('../assets/pepsi-logo.jpg'),
+        branch:'Villa de Alvarezs Branch',
+        work: require('../assets/img/PepsiCo-work-2.jpg'),
+        job:'Truck driver',
+        salary:'MX 1800',
+        date:'Monday to Friday',
+        time:'7:00 am to 3:00 pm',
+    },
+    {
+        id:'3',
+        logo:require('../assets/pepsi-logo.jpg'),
+        branch:'Manzanillos Branch',
+        work: require('../assets/img/PepsiCo-work-3.webp'),
+        job:'Seller',
+        salary:'MX 2200',
+        date:'Monday to Friday',
+        time:'7:00 am to 3:00 pm',
+    },
+
+
+]
+
+
