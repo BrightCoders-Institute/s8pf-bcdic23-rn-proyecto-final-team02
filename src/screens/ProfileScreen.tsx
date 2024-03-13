16734042059be55ef10396b99f89470c12dae45e
Oliver Visoso <ovisosodev@gmail.com>
2024-03-12 18:13:37 -0600

    fix screens design

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,11 +1,23 @@
-import {View, Text} from 'react-native';
+import {View, Platform} from 'react-native';
 import React from 'react';
+import {
+  ContainerComponent,
+  SectionComponent,
+  TextComponent,
+} from '../components';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const ProfileScreen = () => {
+  const {top} = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <ContainerComponent isScroll>
+      <View style={Platform.OS === 'ios' ? {top: top + 15} : {}}>
+        <SectionComponent>
+          <TextComponent text="Profile" font="bold" color="black" size={22} />
+        </SectionComponent>
+      </View>
+    </ContainerComponent>
   );
 };
 
