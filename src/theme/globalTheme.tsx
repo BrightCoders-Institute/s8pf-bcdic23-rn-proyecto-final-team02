bd042241e6a93f549b91ecb7e186e60eedbbf371
Oliver Visoso <ovisosodev@gmail.com>
2024-03-12 18:15:16 -0600

    I kdont now

---
+++ src/theme/globalTheme.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    elevation: 4,
+  },
+});
