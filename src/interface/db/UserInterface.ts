a1266175400519936d153d11696af6e3ea5e49ed
Felix Avalos <felavalosg@gmail.com>
2024-03-28 18:48:37 -0600

    Create interface to DataBase's data
    Create UserInterface to user's database data
    Create CompanyInterface to company's database data

---
+++ src/interface/db/UserInterface.ts
@@ -0,0 +1,23 @@
+export interface User {
+  name: string;
+  last_name: string;
+  email: string;
+  pwd: string;
+  phone: string;
+  address: string;
+  photo: string;
+  working: boolean;
+
+  applications: Application[];
+  docuemtens: Document[];
+}
+
+export interface Application {
+  company: string;
+  state: 'Pending' | 'Rejected' | 'Accepted';
+}
+
+export interface Document {
+  name: 'Curriculum' | 'Aditional';
+  route: string;
+}
