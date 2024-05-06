30bfc870e9e4e411d7fbc84ae179fbba902086fe
Felix Avalos <felavalosg@gmail.com>
2024-05-06 17:01:48 -0600

    Delete useless data

--- src/hook/useAuth.tsx
+++ src/hook/useAuth.tsx
@@ -61,72 +61,8 @@ const useAuth = () => {
 
   // const handleForgetPassword = async () => {
   //   if (email.length > 0) {
-  //     setChangeLoading(true);
-  //     await auth()
-  //       .sendPasswordResetEmail(email)
-  //       .then(() => {
-  //         Alert.alert('Información', 'Correo de recuperación enviado'),
-  //           setChangeLoading(false);
-  //       })
-  //       .catch((err: any) => {
-  //         setChangeLoading(false);
-  //         Alert.alert('Error', `${err.message}`);
-  //       });
+  //     
   //   } else Alert.alert('Alerta', 'Ingresa tu correo electrónico');
-  // };
-
-  // const handleGoogleLogin = async () => {
-  //   try {
-  //     setChangeLoading(true);
-
-  //     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-  //     const {idToken} = await GoogleSignin.signIn();
-  //     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-  //     setChangeLoading(false);
-  //     return auth().signInWithCredential(googleCredential);
-  //   } catch (error: any) {
-  //     setChangeLoading(false);
-  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-  //       Alert.alert('Alerta', 'Inicio de sesión interrumpido');
-  //     } else if (error.code === statusCodes.IN_PROGRESS) {
-  //       Alert.alert('Alerta', 'Inicio de sesión en progreso');
-  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-  //       Alert.alert('Alerta', 'Servicios de google NO DISPLINIBLES');
-  //     } else {
-  //       Alert.alert('Alerta', 'Inicio de sesión fallido');
-  //     }
-  //   }
-  // };
-
-  // const handleGoogleSignUp = async () => {
-  //   try {
-  //     setChangeLoading(true);
-
-  //     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-  //     const {idToken, user} = await GoogleSignin.signIn();
-  //     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-  //     setChangeLoading(false);
-
-  //     return auth()
-  //       .signInWithCredential(googleCredential)
-  //       .finally(() => {
-  //         // Use helper to create user in the firestore
-  //         createUserGoogle(user, auth().currentUser?.uid);
-  //       });
-  //   } catch (error: any) {
-  //     setChangeLoading(false);
-  //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-  //       Alert.alert('Alerta', 'Registro interrumpido');
-  //     } else if (error.code === statusCodes.IN_PROGRESS) {
-  //       Alert.alert('Alerta', 'Registro en progreso');
-  //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-  //       Alert.alert('Alerta', 'Servicios de google NO DISPLINIBLES');
-  //     } else {
-  //       Alert.alert('Alerta', 'Registro fallido');
-  //     }
-  //   }
   // };
 
   const handleEmailOrPhoneChange = ( val: string ) => {
@@ -137,19 +73,6 @@ const useAuth = () => {
       setPhone(val);
     }
   };
-
-  // const signUp = () => {
-  //   // Create user in firebase
-  //   handleCreateUserWithEmail().finally(() => {
-  //     // Save user data in react useState (Object)
-  //     user.email = email;
-  //     user.password = password;
-  //     user.phone = phone;
-
-  //     // Send user Object to firestore
-  //     createUser();
-  //   });
-  // };
 
   return {
     email,
